44e661b9c32cfd841b4a61d3ae86657c8846a748
u88872625 <u88872625@gmail.com>
2024-03-18 12:01:40 +0800

    delete:react-native-dotenv

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,21 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import {
-  REACT_APP_FIREBASE_API_KEY,
-  REACT_APP_FIREBASE_AUTH_DOMAIN,
-  REACT_APP_FIREBASE_PROJECT_ID,
-  REACT_APP_FIREBASE_STORAGE_BUCKET,
-  REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  REACT_APP_FIREBASE_APP_ID,
-} from "@env";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyA9UxRyJqj6PVqoD1bm4QV5IiSzhKRxfT0",
-  authDomain: "meal-planner-ff63a.firebaseapp.com",
-  projectId: "meal-planner-ff63a",
-  storageBucket: "meal-planner-ff63a.appspot.com",
-  messagingSenderId: "858487927874",
-  appId: "1:858487927874:web:1296771a9896ee3c61e9db",
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
