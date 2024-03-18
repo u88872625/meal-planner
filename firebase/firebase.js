7d7b80715606386bd34a27044ea3ec00b49a0f4f
u88872625 <u88872625@gmail.com>
2024-03-18 11:54:09 +0800

    Revert "feat:use environment variable"
    This reverts commit ca52f7805b9dc5ba9cf33e250bd5f57f0b0bc396.

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -10,12 +10,12 @@ import {
 } from "@env";
 
 const firebaseConfig = {
-  apiKey: REACT_APP_FIREBASE_API_KEY,
-  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: REACT_APP_FIREBASE_APP_ID,
+  apiKey: "AIzaSyA9UxRyJqj6PVqoD1bm4QV5IiSzhKRxfT0",
+  authDomain: "meal-planner-ff63a.firebaseapp.com",
+  projectId: "meal-planner-ff63a",
+  storageBucket: "meal-planner-ff63a.appspot.com",
+  messagingSenderId: "858487927874",
+  appId: "1:858487927874:web:1296771a9896ee3c61e9db",
 };
 
 // Initialize Firebase
